73e21656e0722f7d6e7587f94f4862b209186d1c
LeaHaelin <lea8haelin@gmail.com>
2024-11-19 12:50:01 +0100

    Add a prop so it accepts a travelPackage

--- src/app/ProductEditor/page.tsx
+++ src/app/ProductEditor/page.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Package } from "../models/Package";
+import { Country } from "../models/Country";
 
-function ProductEditor() {
+interface ProductEditorProps {
+  travelPackage: Package;
+  country: Country;
+}
+
+function ProductEditor({ travelPackage, country }: ProductEditorProps) {
   return (
     <>
       <div>Product Editor page</div>

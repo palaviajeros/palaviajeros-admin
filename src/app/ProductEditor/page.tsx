5c9017bb56d2714ffab5485a178dde340b447871
Sam Chu <samchupiano@gmail.com>
2024-11-21 12:01:06 +0100

    chore: update ESLint config to prevent build errors for unused vars

--- src/app/ProductEditor/page.tsx
+++ src/app/ProductEditor/page.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Package } from "../models/Package";
-import { Country } from "../models/Country";
+import { Package } from "../models/Package"
+import { Country } from "../models/Country"
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface ProductEditorProps {
-  travelPackage: Package;
-  country: Country;
+  travelPackage: Package
+  country: Country
 }
 
-function ProductEditor({ travelPackage, country }: ProductEditorProps) {
+function ProductEditor() {
   return (
     <>
       <div>Product Editor page</div>
     </>
-  );
+  )
 }
 
-export default ProductEditor;
+export default ProductEditor
